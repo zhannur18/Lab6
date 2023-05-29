5f71638fbfb66962984ce94e72d67a0b4ce2c86f
Zhannur <221952@astanait.edu.kz>
2023-05-29 08:28:04 +0600

    getData

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,7 +1,11 @@
 public class Vertex<V> {
     private V data;
-    private Map<Vertex<V>, Double> adjacent Vertices; // with weights
-    public void add Adjacent Vertex(Vertex<V> destination, double weight) {
-        adjacentVertices.put(destination, weight);
+    private Map<Vertex<V>, Double> adjacent Vertices;
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
     }
-}
+
+    public V getData() {//returns the data associated with this vertex
+        return data;
+    }}
