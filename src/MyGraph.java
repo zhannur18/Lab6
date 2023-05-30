af9608de6a426a3114dd11918cc470b85bf6c192
Zhannur <221952@astanait.edu.kz>
2023-05-30 23:50:25 +0600

    implementing the addVertex method

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,4 +1,10 @@
 public class MyGraph {
     private Map<Vertex<V>, List<Edge<V>>> map; // Map to store vertices and their associated edges
+    public MyGraph() {
+        this.map = new HashMap<>(); // Initializes the map
+    }
 
+    public void addVertex(Vertex<V> vertex) {
+        map.put(vertex, new ArrayList<>()); // Adds a vertex to the map with an empty list of edges
+    }
 }
